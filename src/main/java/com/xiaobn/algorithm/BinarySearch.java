caca12ca12d34f08c041b8ba65f8d6a2a4e0e994
zhangwei112 <zhangwei112@jd.com>
2019-02-27 10:02:26 +0800

    两个面试题

--- src/main/java/com/xiaobn/algorithm/BinarySearch.java
+++ src/main/java/com/xiaobn/algorithm/BinarySearch.java
@@ -31,6 +31,78 @@ public class BinarySearch {
         return -1;
     }
 
+    /**
+     * 变体1  查钊第一个值等于给定的元素
+     * @param a
+     * @param n
+     * @param value
+     * @return
+     */
+    public int bsearch1(int[] a, int n, int value) {
+        int low = 0;
+        int hight = n - 1;
+        while (hight>low){
+            int mid = (hight-low)/2 + 1;
+            if (a[mid] == value){
+                return mid;
+            }
+            if (a[mid] > value){
+                hight = mid-1;
+            }
+            if (a[mid] < value){
+                low = mid + 1;
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * 变体二:查找最后一个值等于给定值的元素
+     * @param a
+     * @param n
+     * @param value
+     * @return
+     */
+    public int bsearch2(int[] a, int n, int value) {
+        int low = 0;
+        int high = n - 1;
+        while (low <= high) {
+            int mid =  low + ((high - low) >> 1);
+            if (a[mid] > value) {
+                high = mid - 1;
+            } else if (a[mid] < value) {
+                low = mid + 1;
+            } else {
+                if ((mid == n - 1) || (a[mid + 1] != value)) return mid;
+                else low = mid + 1;
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * 变体3:查找第一个大于给定值
+     * @param a
+     * @param n
+     * @param value
+     * @return
+     */
+    public int bsearch3(int[] a, int n, int value) {
+        int low = 0;
+        int high = n - 1;
+        while (low <= high) {
+            int mid =  low + ((high - low) >> 1);
+            if (a[mid] >= value) {
+                if ((mid == 0) || (a[mid - 1] < value)) return mid;
+                else high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return -1;
+    }
+
+
     public static void main(String[] args) {
         int[] arr = {6, 12, 33, 87, 90, 97, 108, 561};
         System.out.println("循环查找：" + (binarySearch(arr, 87) + 1));
